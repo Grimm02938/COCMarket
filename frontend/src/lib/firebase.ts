de6721dcb3438759bbe7bd9dc7535ded80b3e75b
Grimm02938 <abdelmoulaadame65@gmail.com>
2025-09-21 23:33:07 +0200

    fix: Restauration du fichier firebase.ts pour corriger le build

---
+++ frontend/src/lib/firebase.ts
@@ -0,0 +1,27 @@
+﻿// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBHCeOGX-2D86NLYdaiSKc4h1VMtFuqZiQ',
+  authDomain: 'cocmarket-0.firebaseapp.com',
+  projectId: 'cocmarket-0',
+  storageBucket: 'cocmarket-0.appspot.com',
+  messagingSenderId: '42792374848',
+  appId: '1:42792374848:web:b062f4f4db04e1e6056507'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+
+// Google Auth Provider
+export const googleProvider = new GoogleAuthProvider();
+
+// Function to sign in with Google
+export const signInWithGoogle = () => {
+  return signInWithPopup(auth, googleProvider);
+};
